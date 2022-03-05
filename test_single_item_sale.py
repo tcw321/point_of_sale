e3e3054aabdae9e2e80a86dbf682e1ab2714dcc1
Timothy C. Wright <tcw321@gmail.com>
2022-03-05 13:19:53 -0500

    reduce duplication in tests

--- test_single_item_sale.py
+++ test_single_item_sale.py
@@ -1,12 +1,16 @@
 import pytest
 from point_of_sale import *
 
-def test_valid_barcode_displays_valid_amount():
-    on_barcode("12345\n", {"12345":"$10.50", "54321":"$8.64"})
+@pytest.fixture
+def catalog():
+    return {"12345": "$10.50", "54321": "$8.64"}
+
+def test_valid_barcode_displays_valid_amount(catalog):
+    on_barcode("12345\n", catalog)
     assert last_text_displayed() == "$10.50"
 
-def test_second_valid_barcode_displays_valid_amount():
-    on_barcode("54321\n", {"12345":"$10.50", "54321":"$8.64"})
+def test_second_valid_barcode_displays_valid_amount(catalog):
+    on_barcode("54321\n", catalog)
     assert last_text_displayed() == "$8.64"
 
 def test_valid_barcode_with_no_price():
