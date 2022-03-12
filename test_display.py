9485812ee3813691775a9c07970c6d7d5ae4239e
Timothy C. Wright <tcw321@gmail.com>
2022-03-12 15:33:42 -0500

    format with single trailing zero.

--- test_display.py
+++ test_display.py
@@ -5,3 +5,8 @@ def test_display_of_two_trailing_zeroes():
     display = Display()
     display.setTotal(2)
     assert display.view() == "Total: $2.00"
+
+def test_display_of_single_trailing_zero():
+    display = Display()
+    display.setTotal(2.2)
+    assert display.view() == "Total: $2.20"
